50c8983d6f6b72313a6835cda3724e09a8611d2b
ahmed <awessam127@gmail.com>
2023-05-12 01:00:06 +0300

    new design for login and register

--- system/testing.java
+++ system/testing.java
@@ -2,11 +2,17 @@ package system;
 
 public class testing {
     public static void main(String[] args) {
-        register test = new register("f", "ouch");
-        if (test.verifyRegistration() == 1) {
-            System.out.println("finally it works");
-        } else {
-            System.out.println("fuck this shit");
-        }
+//        register test = new register("crazy", "ouch");
+//        if (test.verifyRegistration() == 1) {
+//            System.out.println("finally it works");
+//        } else {
+//            System.out.println("fuck this shit");
+//        }
+//        login l = new login("crazy", "ouch");
+//        if (l.verifyLogin()) {
+//            System.out.println("finally it works");
+//        } else {
+//            System.out.println("fuck this shit");
+//        }
     }
 }
